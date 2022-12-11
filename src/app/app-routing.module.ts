0897d9abaf78b593a845ad673eadf305cf2f05de
KonradDzieciol99 <konradd990212@gmail.com>
2022-12-11 21:14:05 +0100

    authGuards changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthModuleGuard } from './core/guards/auth-module.guard';
+import { AuthLoadModuleGuard } from './core/guards/auth-load-module.guard';
+import { AuthGuard } from './core/guards/auth.guard';
 import { HeaderComponent } from './core/header/header.component';
+import { TestComponent } from './test/test/test.component';
 
 const routes: Routes = 
 [
-  {
-    path: '',
-    component : HeaderComponent ,
-    children: [
-      { 
-        canLoad: [AuthModuleGuard],
-        path: 'home', 
-        loadChildren: () => import("./home/home.module").then(mod=>mod.HomeModule)
-      },
-    ]
-  },
   { 
     path: 'auth', 
-    loadChildren: () => import("./authentication/authentication.module").then(mod=>mod.AuthenticationModule)
+    loadChildren: () => import("./authentication/authentication.module").then(mod=>mod.AuthenticationModule),
+    data: { showHeader: false, showSidebar: false,showFooter: false },
   },
+  {
+    canLoad: [AuthLoadModuleGuard],
+    canActivate: [AuthGuard],
+    path: 'home', 
+    loadChildren: () => import("./home/home.module").then(mod=>mod.HomeModule)
+  },
+
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
