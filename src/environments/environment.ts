0f16bb386595899d8d71609ea74915dbeff4539d
KonradDzieciol99 <konradd990212@gmail.com>
2022-12-12 18:24:45 +0100

    fixing overflow bug login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
 
   apiUrl: 'https://localhost:44346/api/',
   COOKIE_REFRESH_TOKEN_NAME:"Workflow-Refresh-Token",
+  AUTO_LOGOUT_TIME_IN_MINUTES:1,
   production: false
 };
 
